b2bd0f12f5f70631714df7d9c2ba685cf223ae0c
e282252 <e282252@edu.sbl.ch>
2023-11-28 16:33:14 +0100

    28.11.2023 Almost done with 1, but I managed to break something

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ export function exercise01(args) {
   }
   return result.join("")
 }
+
+export function exercise01(args) {
+  const input = args
+  const result = []
+ 
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement != "e" && currentElement != "E")
+    result.push(currentElement)
+  }
+  return result.join("")
+}
