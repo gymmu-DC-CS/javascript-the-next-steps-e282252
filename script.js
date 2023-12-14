21f958111d785691f833b99fbab3645a1ee93908
e282252 <e282252@edu.sbl.ch>
2023-12-14 15:39:58 +0100

    end lesson 14.12.2023 finished 3 & 5 with the whole class started 4

--- script.js
+++ script.js
@@ -14,8 +14,42 @@ export function exercise02(args) {
   return args.toUpperCase()
 }
 
-export function exercise02(args) {
-  return args.toUpperCase()
+export function exercise03(args) {
+  const input = args
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === "e") {
+      count = count + 1
+    } else if (currentElement === "E") {
+      count = count + 1
+    }
+  }
+  return count
 }
 
-export function exercise03(args) {}
+export function exercise04(args) {
+  const input = args
+  let count = 0
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i]
+    if (currentElement === " ") {
+      count = count + 1
+    } else if (currentElement === ",") {
+      count = count + 1
+    }
+  }
+  return count
+}
+
+export function exercise05(args) {
+  const input = args
+  for (let i = 0; i < input.length; i++) {
+    const currentElement = input[i].charCodeAt()
+    if (currentElement >= 65 && currentElement <= 90) {
+      return true
+    }
+  }
+
+  return false
+}
